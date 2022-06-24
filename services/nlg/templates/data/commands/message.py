9e353bac3befaf31743785c45c9bf4b5aab31823
Kuan-Yu Lin <linkuanyu1127@gmail.com>
2022-06-24 15:44:20 +0200

    update the full structure of adviser

---
+++ services/nlg/templates/data/commands/message.py
@@ -0,0 +1,74 @@
+###############################################################################
+#
+# Copyright 2020, University of Stuttgart: Institute for Natural Language Processing (IMS)
+#
+# This file is part of Adviser.
+# Adviser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3.
+#
+# Adviser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Adviser.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from typing import List
+import sys
+sys.path.insert(0, '/home/kuanyu/Documents/GitHub/Soup_Advisor/services/nlg/templates/parsing/parsers/codeparser')
+sys.path.insert(0, '/home/kuanyu/Documents/GitHub/Soup_Advisor/services/nlg/templates/parsing/parsers/messageparser')
+sys.path.insert(0, '/home/kuanyu/Documents/GitHub/Soup_Advisor/services/nlg/templates/parsing/parsers/messageparser/data')
+
+from command import Command
+from probability import Probability
+from memory import Memory
+from codeparser import CodeParser
+from messagecomponent import MessageComponent, \
+    MessageComponentType
+from messageparser import MessageParser
+
+MESSAGE_PARSER = MessageParser()
+CODE_PARSER = CodeParser()
+
+
+class Message(Command):
+    def __init__(self, arguments: str):
+        Command.__init__(self, arguments)
+
+        self.components = self._parse_message()
+        self.score = 1.0
+
+    def _parse_message(self) -> List[MessageComponent]:
+        return MESSAGE_PARSER.parse(self.arguments)
+
+    def are_arguments_valid(self) -> bool:
+        return True  # since parse_arguments would have thrown an exception otherwise
+
+    def add_inner_command(self, command):
+        assert isinstance(command, Probability)
+        self.score = command.value
+    
+    def get_normalised_probability(self, score_sum: float) -> float:
+        return self.score / score_sum
+
+    def are_inner_commands_valid(self) -> bool:
+        return len(self.inner_commands) <= 1
+
+    def is_applicable(self, parameters: Memory) -> bool:
+        return True  # messages are always applicable
+
+    def apply(self, parameters: Memory) -> str:
+        output = ''
+        for component in self.components:
+            if component.component_type == MessageComponentType.STRING:
+                output += component.value
+            elif component.component_type == MessageComponentType.ADVISER_CODE or \
+                component.component_type == MessageComponentType.PYTHON_CODE:
+                code = component.value + '$'  # CodeParser expects end of statement
+                expression = CODE_PARSER.parse(code)[0]
+                output += expression.evaluate(parameters)
+        return output
---
+++ utils/logger.py
@@ -0,0 +1,15 @@
+# format copy from adviser.utils.logger 
+# imported by run_chat.py
+# it's not a complete version
+
+import datetime
+import logging
+import os
+import sys
+from enum import IntEnum
+
+class LogLevel(IntEnum):
+    pass
+
+class DiasysLogger(logging.Logger):
+    pass
